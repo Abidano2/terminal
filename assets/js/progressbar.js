c925cc98c0d910f7b5e6550420af27aade0f9ed5
Abidano2 <77632466+Abidano2@users.noreply.github.com>
2021-12-13 22:01:29 +0000

    Update progressbar.js

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -28,3 +28,35 @@ class ProgressBar {
 }
 
 new ProgressBar(document.querySelector('.progress-bar'), 25);
+
+
+class ProgressBar1 {
+	constructor(element, initialValue = 0) {
+		this.valueElem = element.querySelector('.progress-bar-value-ch1');
+		this.fillElem = element.querySelector('.progress-bar-fill-ch1');
+
+		this.setValue(initialValue);
+	}
+
+	setValue(newValue) {
+		if (newValue < 0) {
+			newValue = 0;
+		}
+
+		if (newValue > 100) {
+			newValue = 100;
+		}
+
+		this.value = newValue;
+		this.update();
+	}
+
+	update() {
+		const percentage = this.value + '%';
+
+		this.fillElem.style.width = percentage;
+		this.valueElem.textContent = percentage;
+	}
+}
+
+new ProgressBar1(document.querySelector('.progress-bar-ch1'), 10);
